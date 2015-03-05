f990be983006216b59288bc04925ba613cf96741
Daniel Toader & erica cha <student@epicodus.com>
2015-03-05 14:45:34 -0800

    add new CD input functionality

--- app/app.php
+++ app/app.php
@@ -1,39 +1,34 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Cd.php";
+    require_once __DIR__."/../src/cd.php";
+
+    session_start();
+    if (empty($_SESSION['cds'])) {
+        $_SESSION['cds'] = [];
+        require_once __DIR__."/../src/cdData.php";
+    }
 
     $app = new Silex\Application();
+    // Registerung twig in Silex
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get("/home", function() {
-        return "Home";
+    $app->get("/", function() use ($app) {
+       return $app['twig']->render('cdList.twig', array('cds' => CD::getCDs()));
     });
 
-    $app = new Silex\Application();
+    $app->get("/cdInput", function() use ($app) {
+        return $app['twig']->render('cdInput.twig');
+    });
 
+    $app->post("/cdInputed", function() use ($app) {
+        $newCD = new CD($_POST['title'], $_POST['artist'], $_POST['cover_art'], $_POST['price']);
+        $newCD->save();
+        return $app['twig']->render('cdInputed.twig', array('album' => $newCD));;
+    });
 
-   $app->get("/", function() {
-       $first_cd = new CD("Master of Reality", "Black Sabbath", "images/reality.jpg", 10.99);
-       $second_cd = new CD("Electric Ladyland", "Jimi Hendrix", "images/ladyland.jpg", 10.99);
-       $third_cd = new CD("Nevermind", "Nirvana", "images/nevermind.jpeg", 10.99);
-       $fourth_cd = new CD("I don't get it", "Pork Lion", "images/porklion.jpg", 49.99);
-       $cds = array($first_cd, $second_cd, $third_cd, $fourth_cd);
-
-       $output = "";
-       foreach ($cds as $album) {
-           $output = $output . "<div class='row'>
-               <div class='col-md-6'>
-                   <img src=" . $album->getCoverArt() . ">
-               </div>
-               <div class='col-md-6'>
-                   <p>" . $album->getTitle() . "</p>
-                   <p>By " . $album->getArtist() . "</p>
-                   <p>$" . $album->getPrice() . "</p>
-               </div>
-           </div>
-           ";
-       }
-       return $output;
-   });
+    $app->get("/searchbyartist", function() use ($app) {
+        return $app['twig']->render('search.twig');
+    });
 
     return $app;
 ?>
